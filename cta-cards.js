fa076b95560912fcd1eba3d82514e5febf851aa4
ottomen <a.shevchenko@makebecool.com>
2017-05-12 14:42:44 +0300

    adeed

--- cta-cards.js
+++ cta-cards.js
@@ -6,13 +6,7 @@
 
 (function (window, videojs) {
   var defaults = {
-      image_url: null,
-      click_url: '',
-      start_time: null,
-      end_time: null,
-      opacity: 0.7,
-      height: '100%',
-      width: '100%'
+      customClass: 'card-aj'
     },
     ctaCard = function (options) {
       var player = this,
@@ -20,13 +14,34 @@
 
       ctaCardInstance = {
         getCards: function () {
-          console.log('Ajax cart request');
+
+          var url = settings.cardUrl;
+
+          console.log('Ajax cart request', url);
+
+          this.deleteCardDOM();
+          this.addCardDOM();
         },
         deleteCardDOM: function () {
-
+          console.log('deleteCardDOM');
         },
         addCardDOM: function () {
+          console.log('addCardDOM');
+          var template = '<div class="vjs-cta-detail active">';
+          template += '<a href="" target="_blank" class="vjs-cta-js-detail">';
 
+          template += '<div>';
+          template += '<img src="" class="detail-image">';
+          template += '<h3 class="title"></h3>';
+          template += '<div class="price-set js-if-price">';
+          template += '<del class="original-price js-original-price"></del>';
+          template += '<span class="price">$</span>';
+          template += '</div>';
+          template += '<span class="description"></span>';
+          template += '</div>';
+          template += '</a>';
+          template += '</div>';
+          console.log('Template', template);
         }
       };
 
@@ -34,7 +49,7 @@
         settings.start_time = 0;
 
       player.on('timeupdate', ctaCardInstance.getCards);
-      
+
       console.log('Player', player);
     };
 
