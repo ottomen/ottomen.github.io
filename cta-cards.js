4de3e94b91e927f9d10bcd7da901faad3949b6e7
ottomen <a.shevchenko@makebecool.com>
2017-05-12 14:26:03 +0300

    adeed

--- cta-cards.js
+++ cta-cards.js
@@ -4,27 +4,37 @@
  *  https://github.com/funnyordie/videojs-ctaCard/blob/master/LICENSE.md
  */
 
-(function(window, videojs) {
+(function (window, videojs) {
   var defaults = {
       image_url: null,
-      click_url:     '',
+      click_url: '',
       start_time: null,
       end_time: null,
       opacity: 0.7,
       height: '100%',
       width: '100%'
     },
-    ctaCard = function(options) {
+    ctaCard = function (options) {
       var player = this,
         settings = videojs.mergeOptions(defaults, options);
+
+      ctaCardInstance = {
+        getCards: function () {
+          console.log('Ajax cart request');
+        },
+        deleteCardDOM: function () {
+
+        },
+        addCardDOM: function () {
+
+        }
+      };
 
       if (settings.start_time === null)
         settings.start_time = 0;
 
-      //player.on('timeupdate', overlay.checkOverlay);
-      //player.on('loadedmetadata', overlay.checkEndTime);
-
-
+      player.on('timeupdate', ctaCardInstance.getCards);
+      
       console.log('Player', player);
     };
 
